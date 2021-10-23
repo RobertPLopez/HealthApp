94337a8966d7ece18e6790ba6070bbe9f0b2241a
Robert_Lopez <rplopez720@gmail.com>
2021-10-23 09:15:32 -0400

    DATA

---
+++ HealthApp.Data/PrimaryTableFitness.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthApp.Data
+{
+    class PrimaryTableFitness
+    {
+        [Key]
+        [Display(Name = "Your Fitness Plan!")]
+        public Guid MyFitnessPlan { get; set; }
+        [Required]
+        [Display(Name = "Your Workout! Own it!")]
+        public string TypeOfWorkout { get; set; }
+        [Required]
+        [Display(Name = "This is the total number of calories burned!")]
+        public int CaloriesBurned { get; set; }
+    }
+}
