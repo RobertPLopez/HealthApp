ec1b2e9bddfc055bfa56304219ba09799c48e151
Robert_Lopez <rplopez720@gmail.com>
2021-10-23 11:16:59 -0400

    NOTESERVICE

---
+++ HealthApp.Services/PrimaryTableFitnessService.cs
@@ -0,0 +1,58 @@
+﻿using HealthApp.Models.PrimaryTableFitness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthApp.Services
+{
+    public class PrimaryTableFitnessService
+    {
+        private readonly Guid _userId;
+        public PrimaryTableFitnessService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreatePrimaryFitnessTable(PrimaryTableFitnessCreate model)
+        {
+            var entity =
+                new PrimaryTableFitness()
+                {
+                    OwnerId = _userId,
+                    Workout = model.TypeOfWorkout,
+                    DailyCaloriesBurned = model.CaloriesBurned,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.FitnessTables.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<PrimaryTableFitnessListItem> GetPrimaryTableFitness()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .FitnessTables
+                    .Where(e => e.OwnerId == _userId)
+                    .Select(
+                        e => 
+                        new PrimaryTableFitnessListItem
+                        {
+                            MyFitnessPlan = e.MyFitnessPlan,
+                            TypeOfWorkout = e.TypeOfWorkout,
+                            CaloriesBurned = e.CaloriesBurned, 
+                            CreatedUtc = e.CreatedUtc
+                        }
+                    );
+                return query.ToArray();
+            }
+        }
+    }
+}
