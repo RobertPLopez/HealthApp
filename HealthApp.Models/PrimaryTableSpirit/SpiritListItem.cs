466f07e55df1c82390219f1985fa6e0240b386e8
Robert_Lopez <rplopez720@gmail.com>
2021-10-26 19:35:08 -0400

    Spirit ListItem Model

--- HealthApp.Models/PrimaryTableSpirit/SpiritListItem.cs
+++ HealthApp.Models/PrimaryTableSpirit/SpiritListItem.cs
@@ -8,5 +8,18 @@ namespace HealthApp.Models.PrimaryTableSpirit
 {
     class SpiritListItem
     {
+        public int HowIViewMe { get; set; }
+
+        public int HowIViewOthers { get; set; }
+
+        public int HowOtherPerceiveMe { get; set; }
+
+        public string MySocialCircle { get; set; }
+
+        public int MyRestHours { get; set; }
+        public string OutsideMotivation { get; set; }
+        public string InternalMotivaiton { get; set; }
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
     }
 }
