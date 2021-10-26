0ac5628d315e6a72289effbd46a548602ff786da
Robert_Lopez <rplopez720@gmail.com>
2021-10-26 19:28:02 -0400

    Spirit edit model

--- HealthApp.Models/PrimaryTableSpirit/SpiritEdit.cs
+++ HealthApp.Models/PrimaryTableSpirit/SpiritEdit.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace HealthApp.Models.PrimaryTableSpirit
 {
-    class SpiritEdit
+    public class SpiritEdit
     {
+        
+        public int HowIViewMe { get; set; }
+        
+        public int HowIViewOthers { get; set; }
+        
+        public int HowOtherPerceiveMe { get; set; }
+        
+        public string MySocialCircle { get; set; }
+        
+        public int MyRestHours { get; set; }
+        
+        public string OutsideMotivation { get; set; }
+       
+        public string InternalMotivaiton { get; set; }
+        
+        public DateTimeOffset CreatedUtc { get; set; }
     }
 }
+
