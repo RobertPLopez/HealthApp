f9daddd2ed71d326380c75b3c009560e25e25257
Robert_Lopez <rplopez720@gmail.com>
2021-10-28 18:38:37 -0400

    Controller

--- HealthApp.WebMVC/Controllers/PrimaryTableSpiritController.cs
+++ HealthApp.WebMVC/Controllers/PrimaryTableSpiritController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HealthApp.Models.PrimaryTableSpirit;
+using HealthApp.Services;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,81 @@ namespace HealthApp.WebMVC.Controllers
 {
     public class PrimaryTableSpiritController : Controller
     {
-        // GET: PrimaryTableSpirit
+        [Authorize]
+        // GET: PrimaryTableSpirit Indez
         public ActionResult Index()
         {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new PrimaryTableSpiritService(userId);
+            var model = service.GetPrimarySpiritTable();
+
+            return View(model);
+        }
+
+        //Get: PrimaryTableSpirit Create
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        //Get: PrimaryTableSpirit Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateSpiritTable(SpiritCreate model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var service = CreateSpiritTableServices();
+
+            if (service.CreateSpiritTable(model))
+            {
+                TempData["SaveResault"] = "Your spirit plan was saved!";
+                return RedirectToAction("Index");
+            };
+            ModelState.AddModelError("", "Your spirit plan could not be created.");
+
+            return View(model);
+        }
+
+        //Get:PrimarySpiritTable Details
+        public ActionResult
+            PrimaryTableSpiritDetails(int id)
+        {
+            var svc = CreateSpiritTableServices();
+            var model = svc.GetPrimaryTableSpiritById(id);
+
+            return View(model);
+        }
+        //Get: PrimarySpiritTable Delete
+        [ActionName("Delete")]
+        public ActionResult PrimaryTableSpiritDelete(int id)
+        {
+            var svc = CreateSpiritTableServices();
+            var model = svc.GetPrimaryTableSpiritById(id);
+
+            return View(model);
+        }
+
+        //Get: PrimarySpiritTable Delete Overload
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeletePost(int id)
+        {
+            var service = CreateSpiritTableServices();
+
+            service.PrimaryTableSpiritDelete(id);
+
+            TempData["SaveREsult"] = "Your plan was deleted";
+
+            return RedirectToAction("Index");
+        }
+
+        private PrimaryTableSpiritService CreateSpiritTableServices()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new PrimaryTableSpiritService(userId);
+            return service;
         }
     }
 }
