8b919225aca7bc23c7bd72f905b6460342534703
Robert_Lopez <rplopez720@gmail.com>
2021-10-28 18:57:22 -0400

    Food list item

--- HealthApp.Models/PrimaryTableFood/FoodListItem.cs
+++ HealthApp.Models/PrimaryTableFood/FoodListItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,19 @@ namespace HealthApp.Models.PrimaryTableFood
 {
     class FoodListItem
     {
+        [Key]
+        public int FoodId { get; set; }
+        [Required]
+        public string FoodName { get; set; }
+        [Required]
+        public decimal TotalCaloriesConsumed { get; set; }
+        [Required]
+        public string FoodContent { get; set; }
+        [Required]
+        public decimal DailyWeight { get; set; }
+        [Required]
+        public int CupsWaterDrank { get; set; }
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
     }
 }
